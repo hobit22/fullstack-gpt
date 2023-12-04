19941a33c0a29fb3f9b3b14bb45711e317c8e122
hobit22 <hobit22@naver.com>
2023-12-04 22:27:04 +0900

    7.2 data flow

--- Home.py
+++ Home.py
@@ -1,9 +1,30 @@
 import streamlit as st
+from datetime import datetime
+from langchain.prompts import PromptTemplate
 
-st.title("hello worold!")
+today = datetime.today().strftime("%H:%M:%S")
 
-st.subheader("Welcome to StreamLit")
+st.title(today)
 
-st.markdown("""
-    ### I love it!
-""")
+model = st.selectbox(
+    "Choose your model", 
+    ("GPT-3", "GPT-4")
+)
+
+if model == 'GPT-3':
+    st.write("cheap")
+else:
+    st.write("not cheap")
+    
+    name = st.text_input("what is your name?")
+
+    st.write(name)
+
+
+    value = st.slider(
+        "temperature", 
+        min_value=0.0, 
+        max_value=2.0,
+        step=0.1
+    )
+

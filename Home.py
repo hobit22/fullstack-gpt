0eadda6c264ca822ed1e09fd7a5478bb2ac7246e
hobit22 <hobit22@naver.com>
2023-12-04 22:52:07 +0900

    7.3 Multi page

--- Home.py
+++ Home.py
@@ -1,30 +1,9 @@
 import streamlit as st
-from datetime import datetime
-from langchain.prompts import PromptTemplate
 
-today = datetime.today().strftime("%H:%M:%S")
-
-st.title(today)
-
-model = st.selectbox(
-    "Choose your model", 
-    ("GPT-3", "GPT-4")
+st.set_page_config(
+    page_title="FullStack GPT Home",
+    page_icon="😀",
 )
 
-if model == 'GPT-3':
-    st.write("cheap")
-else:
-    st.write("not cheap")
-    
-    name = st.text_input("what is your name?")
-
-    st.write(name)
-
-
-    value = st.slider(
-        "temperature", 
-        min_value=0.0, 
-        max_value=2.0,
-        step=0.1
-    )
+st.title("FullStack GPT Home")
 

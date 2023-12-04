deae39be98c813c564f223c888af3e2cb1855206
hobit22 <hobit22@naver.com>
2023-12-04 23:59:30 +0900

    7.6 Uploading Documents

--- pages/02_PrivateGPT.py
+++ pages/02_PrivateGPT.py
@@ -1,13 +1,8 @@
 import streamlit as st
-from langchain.document_loaders import AsyncHtmlLoader
-from langchain.document_transformers import Html2TextTransformer
 
-urls = ["https://python.langchain.com/docs/integrations/document_transformers/html2text"]
-loader = AsyncHtmlLoader(urls)
-docs = loader.load()
+st.set_page_config(
+    page_title="PrivateGPT",
+    page_icon="📃",
+)
 
-html2text = Html2TextTransformer()
-docs_transformed = html2text.transform_documents(docs)
-
-docs_transformed[0].page_content[1000:2000]
-
+st.title("PrivateGPT")

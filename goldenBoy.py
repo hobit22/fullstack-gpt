003fa1caa4a21117effad92ae74541b281a91f8b
hobit22 <hobit22@naver.com>
2023-12-21 23:30:04 +0900

    prompt 변경

--- goldenBoy.py
+++ goldenBoy.py
@@ -33,6 +33,7 @@ file_path = "./files/question_01.csv"
 parsed_data = load_csv_to_list(file_path)
 
 llm = ChatOpenAI(
+    model="gpt-3.5-turbo-1106",
     temperature=0.1,
     streaming=True,
     callbacks=[
@@ -52,9 +53,20 @@ prompt = FewShotPromptTemplate(
         Solving : {solving}
         Answer : {answer}
 
-        당신은 친절한 수학 선생님입니다.
-        무조건 한글로 대답해야해.
-        단계별로 풀어서 설명해줘.
+        through the process of solving a math problem in an interactive and engaging manner. 
+
+        ### Instructions: 
+        1. Use Korean as the language of instruction.
+        2. Break down the problem step by step, ensuring clarity at each stage.
+        3. Encourage the student to actively participate by asking them questions and seeking their input.
+        4. Provide relevant examples and real-life applications to help the student understand the concept.
+        5. Help the student develop problem-solving skills by guiding them to arrive at the solution independently.
+
+        Context:
+        You are working with a student who requires assistance in solving a math problem. The student is struggling with understanding the steps involved and requires a patient and comprehensive explanation in Korean. Your goal is to guide the student through each step, ensuring complete understanding and clarity.
+
+        Outcome:
+        The student should gain a thorough understanding of the math problem and should be able to solve similar problems independently..
     """
     ,
     input_variables=["question", "solving", "answer"],
@@ -63,7 +75,7 @@ prompt = FewShotPromptTemplate(
 
 chain = prompt | llm
 
-for item in parsed_data:
+for item in parsed_data[:5]:
     id = item["id"]
     question = item["test"]
     solving = item["expText"]
